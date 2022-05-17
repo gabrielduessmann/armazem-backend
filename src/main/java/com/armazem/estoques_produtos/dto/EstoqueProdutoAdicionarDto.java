9eac379d97a81fae01c33fd95522e7f3285d24af
Gabriel Duessmann <gabriel.duessmann@gmail.com>
2022-05-16 23:36:39 -0300

    Criado ações de EstoqueProduto

---
+++ src/main/java/com/armazem/estoques_produtos/dto/EstoqueProdutoAdicionarDto.java
@@ -0,0 +1,11 @@
+package com.armazem.estoques_produtos.dto;
+
+import java.util.UUID;
+
+public class EstoqueProdutoAdicionarDto {
+    public UUID estoqueId;
+
+    public UUID produtoId;
+
+    public Integer quantidade;
+}
